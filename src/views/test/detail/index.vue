b5605227312d5341c073cbc4c1302829d76dc297
zyh940305858 <940305858@qq.com>
2019-12-25 07:28:17 +0800

    查看试题

--- src/views/test/detail/index.vue
+++ src/views/test/detail/index.vue
@@ -2,11 +2,26 @@
   <div class="test-check-detail">
     <h2 class="test-check-detail-title">试题详情</h2>
     <div class="test-check-detail-main">
-      <div class="test-check-detail-content">
-        <div><span>{{ detaildata[0].title }}</span></div>
+      <div class="test-check-detail-list">
+        <div style="margin-bottom: 20px;"><span>出题人：{{ testdetail.user_name }}</span></div>
+        <h3>题目信息</h3>
+        <div style="margin-top: 20px; margin-bottom: 20px;">
+          <el-tag>{{ testdetail.questions_type_text }}</el-tag>
+          <el-tag type="warning">{{ testdetail.subject_text }}</el-tag>
+          <el-tag type="danger">{{ testdetail.exam_name }}</el-tag>
+        </div>
+        <h4>{{ testdetail.title }}</h4>
+        <div>
+          <div class="markdown">
+            <p>{{ testdetail.questions_stem }}</p>
+          </div>
+        </div>
       </div>
-      <div class="test-check-detail-content">
-        123
+      <div class="test-check-detail-answer">
+        <h3>答案信息</h3>
+        <div class="markdown">
+          <p>{{ testdetail.questions_answer }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -18,7 +33,7 @@ import { mapState, mapActions } from 'vuex'
 export default {
   data() {
     return {
-
+      testdetail: {}
     }
   },
   computed: {
@@ -31,6 +46,7 @@ export default {
       questions_id: this.$route.query.id
     }
     await this.getdetailtest(obj)
+    this.testdetail = this.detaildata[0]
   },
   methods: {
     ...mapActions({
